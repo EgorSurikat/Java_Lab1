f1a78dfafc637141f60a5593371ab9672b360d45
Egor <dementev.eg@yandex.ru>
2023-02-15 19:50:28 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+//        ComplexIO start = new ComplexIO();
+//        start.WorkWithComplex();
+        MatrixIO start = new MatrixIO();
+        start.WorkWithMatrix();
+//        InputOutput start = new InputOutput();
+//        start.startWork();
+//        Complex[] new_m = new Complex[10];
+//        System.out.println(new_m.length);
+//        Matrix a = new Matrix(2, 2);
+//        Complex[][] lst = new Complex[2][2];
+//        Complex[][] dataList = new Complex[][]{{new Complex(1, 0), new Complex(2, 2)}, {new Complex(3, -1.5), new Complex(4,0)}};
+//        for (int i = 0; i < 2; i++)
+//            for (int j = 0; j < 2; j++)
+//                lst[i][j] = dataList[i][j];
+//        a.FillMatrix(lst);
+//        a.CalcDet().PrintComplex();
+//        System.out.println();
+//        a.printMatrix();
+    }
+}
